ca1d25941a3255cf2257ab83b70b738003d6aa7f
unknown <s821303@SE1.swedom.virt>
2015-01-07 16:10:42 +0100

    class verstopfung added

---
+++ src/Verstopfung.java
@@ -0,0 +1,14 @@
+import nodenet.InputChannelVector;
+import nodenet.NodeBehavior;
+import nodenet.OutputChannelVector;
+
+
+public class Verstopfung implements NodeBehavior {
+
+	@Override
+	public void transmitPacket(InputChannelVector inputChannels, OutputChannelVector outputChannels) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
