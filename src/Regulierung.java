6affa00ff573c820d8287f8a8a8a024b34efdaba
schubalibre <robertodziuba@googlemail.com>
2015-01-11 00:56:32 +0100

    aufgabe 10 erledigt

--- src/Regulierung.java
+++ src/Regulierung.java
@@ -1,3 +1,6 @@
+import nodenet.ChannelDisabledException;
+import nodenet.ChannelEmptyException;
+import nodenet.ChannelFullException;
 import nodenet.InputChannelVector;
 import nodenet.NodeBehavior;
 import nodenet.OutputChannelVector;
@@ -5,11 +8,44 @@ import nodenet.OutputChannelVector;
 
 public class Regulierung implements NodeBehavior {
 
-	@Override
-	public void transmitPacket(InputChannelVector inputChannels,
-			OutputChannelVector outputChannels) {
-		// TODO Auto-generated method stub
+	private Object obj = null;
+	// die Zeiger der Input Channel's und der Output Channel's
+	private	int next = -1, last = 0, count = 0;
 
+	@Override
+	public void transmitPacket(InputChannelVector inputChannels, OutputChannelVector outputChannels) {
+		
+		// die Channelanzahl (Bei jedem Durchlauf aktualisiert )
+		int inputSize = inputChannels.size();
+		int outputSize = outputChannels.size();
+
+		// Wenn keine Channel da sing
+		if(inputSize == 0 || outputSize == 0)return;
+
+		// Zeiger meines Inputchannels - da modulo ist es egal ob er bei 0 anfängt
+		if(next < 0) next = (int) (Math.random() * inputSize);
+		
+		try{
+			this.obj =  inputChannels.elementAt(next).readObject(); 
+		}
+		catch(ChannelEmptyException exc) { return; } 
+		catch(ChannelDisabledException exc2) { return; }
+		
+		count++;
+		
+		if(count % 3 == 0){
+			// Zeiger meines Outputchannels - da modulo ist es egal ob er bei 0 anfängt
+			last = (last+1) % outputSize;
+			
+			try{
+				outputChannels.elementAt(last).writeObject(this.obj); 
+			}
+			catch(ChannelFullException exc) { return; } 
+			catch(ChannelDisabledException exc2) { return; }
+		}
+		
+		
+			
 	}
 
 }
